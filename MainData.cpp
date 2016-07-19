6a3273094a8a64043c987a7ec588c6a0c42026e3
Motoharu SOMEYA <maretol@outlook.com>
2016-07-20 00:00:35 +0900

    add init

---
+++ MainData.cpp
@@ -0,0 +1,24 @@
+#include "MainData.h"
+#include <stdio.h>
+
+bool vrMain::Init() {
+	vr::EVRInitError eError = vr::VRInitError_None;
+	hmd = vr::VR_Init(&eError, vr::VRApplication_Scene);
+
+	if (eError != vr::VRInitError_None) {
+		printf("error");
+		return false;
+	}
+
+	return true;
+}
+
+void vrMain::Shutdown() {
+}
+
+
+int main() {
+	vrMain *vrmain = new vrMain();
+	vrmain->Init();
+	return 0;
+}
