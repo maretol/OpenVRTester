11e115f3b936a5b21bf7e96d51fcb5015cda1909
Motoharu SOMEYA <maretol@outlook.com>
2016-07-21 16:13:56 +0900

    add constractor and comment

--- RenderModel.cpp
+++ RenderModel.cpp
@@ -1,16 +1,37 @@
 #include "RenderModel.h"
 
-// 本来 OpenGL で書くんだって（てきとう
+// 本来 OpenGL で書くもの
+// Sample の CGLRenderModelに該当
 
-RenderModel::RenderModel(const string &renderModelName) {}
+RenderModel::RenderModel(const string &renderModelName) : m_ModelName(renderModelName) {
+	m_IndexBuffer = 0;
+	m_VertArray = 0;
+	m_VertBuffer = 0;
+	m_Texture = 0;
+}
 
 
-RenderModel::~RenderModel() {}
+RenderModel::~RenderModel() {
+	Cleanup();
+}
 
+// 初期化
+// ただどこまで実装できるかわからんので今は後回し
 bool RenderModel::BInit(const vr::RenderModel_t & vrModel, const vr::RenderModel_TextureMap_t & vrDiffuseTexture) {
 	return false;
 }
 
-void RenderModel::Cleanup() {}
+void RenderModel::Cleanup() {
+	if (m_VertBuffer) {
+		// Buffer, Arrayの開放
+		m_IndexBuffer = 0;
+		m_VertArray = 0;
+		m_VertBuffer = 0;
+	}
+}
 
-void RenderModel::Draw() {}
+// 描画
+// GL でない実装なので後回し
+void RenderModel::Draw() {
+
+}
