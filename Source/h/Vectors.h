f2b3fde6ded672abe45e427017e86e807d6b505a
Motoharu SOMEYA <maretol@outlook.com>
2016-08-22 01:14:53 +0900

    finish to add vector2 inline function

--- Source/h/Vectors.h
+++ Source/h/Vectors.h
@@ -135,4 +135,116 @@ inline Vector2 Vector2::operator+(const Vector2& rhs) const {
 	return Vector2(x + rhs.x, y + rhs.y);
 }
 
+inline Vector2 Vector2::operator-(const Vector2& rhs) const {
+	return Vector2(x - rhs.x, y - rhs.y);
+}
+
+inline Vector2& Vector2::operator+=(const Vector2& rhs) {
+	x += rhs.x;
+	y += rhs.y;
+	return *this;
+}
+
+inline Vector2& Vector2::operator-=(const Vector2& rhs) {
+	x -= rhs.x;
+	y -= rhs.y;
+	return *this;
+}
+
+inline Vector2 Vector2::operator*(const float a) const {
+	return Vector2(x*a, y*a);
+}
+
+inline Vector2 Vector2::operator*(const Vector2 & rhs) const {
+	return Vector2(x*rhs.x, y*rhs.y);
+}
+
+inline Vector2& Vector2::operator*=(const float a) {
+	x *= a;
+	y *= a;
+	return *this;
+}
+
+inline Vector2 & Vector2::operator*=(const Vector2 & rhs) {
+	x *= rhs.x;
+	y *= rhs.y;
+	return *this;
+}
+
+inline Vector2 Vector2::operator/(const float a) const {
+	return Vector2(x / a, y / a);
+}
+
+inline Vector2 & Vector2::operator/=(const float a) {
+	x /= a;
+	y /= a;
+	return *this;
+}
+
+inline bool Vector2::operator==(const Vector2 & rhs) const {
+	return (x == rhs.x) && (y == rhs.y);
+}
+
+inline bool Vector2::operator!=(const Vector2 & rhs) const {
+	return (x != rhs.x) || (y != rhs.y);
+}
+
+inline bool Vector2::operator<(const Vector2 & rhs) const {
+	if (x < rhs.x) { return true;  }
+	if (x > rhs.x) { return false; }
+	if (y < rhs.y) { return true;  }
+	if (y > rhs.y) { return false; }
+	return false;
+}
+
+inline float Vector2::operator[](int index) const {
+	return (&x)[index];
+}
+
+inline float& Vector2::operator[](int index) {
+	return (&x)[index];
+}
+
+inline void Vector2::set(float x, float y) {
+	this->x = x;
+	this->y = y;
+}
+
+inline float Vector2::length()const {
+	return sqrtf(x*x + y*y);
+}
+
+inline float Vector2::distance(const Vector2& vec) const {
+	return sqrtf((vec.x - x)*(vec.x - x) + (vec.y - y)*(vec.y - y));
+}
+
+inline Vector2& Vector2::normalize() {
+	float xxyy = x*x + y*y;
+
+	float invLength = 1.0f / sqrt(xxyy);
+	x *= invLength;
+	y *= invLength;
+	return *this;
+}
+
+inline float Vector2::dot(const Vector2& rhs) const {
+	return(x*rhs.x + y*rhs.y);
+}
+
+inline bool Vector2::equal(const Vector2& rhs, float epsilon) const {
+	return fabs(x - rhs.x) < epsilon && fabs(y - rhs.y) < epsilon;
+}
+
+inline Vector2 operator*(const float a, const Vector2 vec) {
+	return Vector2(a*vec.x, a*vec.y);
+}
+
+inline std::ostream& operator<<(std::ostream& os, const Vector2& vec) {
+	os << "(" << vec.x << "," << vec.y << ")";
+	return os;
+}
+
+// End of inline function for Vector2 
+
+
 #endif // !VECTORS_H_DEF
